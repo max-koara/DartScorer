6e0ff385c8ba7b101db089e120e2bae7bdaa2e5f
max-koara <matelediter@gmail.com>
2020-07-13 01:36:21 +0900

    check: email verification tutorial

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
-Route::get('/', function () {
+Auth::routes(['verify' => true]);
+Route::get('/home', 'HomeController@index')->middleware('verified');
+
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*')->middleware('verified');
 
-Auth::routes();
+//Auth::routes();
+//Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
